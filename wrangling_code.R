9500be84ff9d3846594ca677487fccda82f3559e
Franky Zhang <zhangyifan@dhcp-wifi-8021x-155-41-124-196.bu.edu>
2021-10-08 16:46:46 -0400

    update

--- wrangling_code.R
+++ wrangling_code.R
@@ -7,22 +7,37 @@ library(data.table)
 # including 195 countries
 # data_1: lifer expectancy in different countries from 1799 to 2049(expected)
 # data_2: income per person(gdp) in different countries from 1799 to 2049(expected)
+#### test
 data_1 <- read.csv("https://raw.githubusercontent.com/MA615-Yifan/MA615_hw2/main/life_expectancy_years.csv",
                    header = TRUE)
 data_1 <- data_1[,1:252]
 data_2 <- read.csv("https://raw.githubusercontent.com/MA615-Yifan/MA615_hw2/main/income_per_person_gdppercapita_ppp_inflation_adjusted.csv",
                    header = TRUE)
+
 country_n <- dim(data_1)[1]
 year_n <- dim(data_1)[2] - 1
 # data tidy
-sorted_1 <- data_1 %>%
-  pivot_longer(-country, names_to = "year", values_to = "life_expectancy")
-sorted_1$year <- as.numeric(sub('X', '', sorted_1$year))
-sorted_1$life_expectancy <- as.numeric(sorted_1$life_expectancy)
+# construct different indicators
+country <- vector()
+for (i in 1: country_n) {
+  country <- c(country, rep(data_1$country[i], year_n))
+}
+life_expect <- vector()
+for (i in 1: country_n){
+  a <- data_1[i, ]
+  b <- unlist(a)
+  life_expect <- c(life_expect, as.numeric(b[2:(year_n + 1)]))
+}
+income_per_person <- vector()
+for (i in 1: country_n) {
+  a <- data_2[i, ]
+  b <- unlist(a)
+  income_per_person <- c(income_per_person, as.numeric(
+    sub("k", "e3", b[2:(year_n + 1)], fixed = TRUE)))
+}
+year <- as.numeric(rep(c(1799:2049), country_n))
+data <- data.frame(country, year, life_expect, income_per_person)
 
-data_2 <- data.frame(lapply(data_2, as.character), stringsAsFactors=FALSE)
-sorted_2 <- data_2 %>%
-  pivot_longer(-country, names_to = "year", values_to = "income_per_person")
-sorted_2$year <- as.numeric(sub('X', '', sorted_1$year))
-sorted_2$income_per_person <- as.numeric(sub("k", "e3", sorted_2$income_per_person))
-data <- cbind(sorted_1, sorted_2[, 3])
+# filtering a column to see whether the data with "k" has been successfully converted
+# data %>%
+#   filter(country == "Australia")
